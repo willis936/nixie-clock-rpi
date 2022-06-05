55f9aa873a5c06e87229ed51df933e1531b5e6f9
Paul Willis <paul@willis936.net>
2022-06-05 14:35:03 -0400

    switch back to strange batched stdout reads

--- nixieDriver.py
+++ nixieDriver.py
@@ -2,11 +2,8 @@ import RPi.GPIO as GPIO
 import pigpio
 import time, datetime
 import os, sys, signal, subprocess, threading, gc
-from queue import Queue, Empty
 from contextlib import contextmanager,redirect_stderr,redirect_stdout
 import math, numpy
-
-ON_POSIX = 'posix' in sys.builtin_module_names
 
 # constants
 
@@ -80,11 +77,6 @@ def suppress_stdout_stderr():
     with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
       yield (err, out)
 
-def enqueue_output(out, queue):
-  for line in iter(out.readline, b''):
-    queue.put(line.decode('utf-8'))
-  out.close()
-
 def initDriver():
   waitForPigpio()
   GPIO.setmode(GPIO.BOARD)
@@ -152,36 +144,23 @@ def drivePPSOut():
   print("Starting PPS driving thread.")
 
   strCall = ["sudo","chrt","--rr","99","pps-out","-g",str(pinStrobe),"-e",str(round(tPreEmpt  *1E6)),"-m",str(round(0.1*1E6)),"-l","1","-s","1"]
-  ppsOutProcess = subprocess.Popen(strCall, stdout=subprocess.PIPE, close_fds=ON_POSIX)
-
-  q = Queue()
-  t = threading.Thread(target=enqueue_output, args=(ppsOutProcess.stdout, q))
-  t.daemon = True # thread dies with the program
-  t.start()
+  ppsOutProcess = subprocess.Popen(strCall, stdout=subprocess.PIPE)
 
   while True:
     # collect timing error stats
     time.sleep(0.5)
 
-    try:  line = q.get_nowait() # or q.get(timeout=.1)
-    except Empty:
-      #print("Nada")
-      continue
-    else: # got line
-      print(line)
+    line = ppsOutProcess.stdout.readline().rstrip()
 
     try:
       line = [int(i) for i in line.split()]
     except:
-      print("Fail")
       continue
       pass
     else:
-      print(len(line))
       pass
 
     if len(line) != 5:
-      time.sleep(0.5)
       continue
 
     tErrTmp = line[2]
